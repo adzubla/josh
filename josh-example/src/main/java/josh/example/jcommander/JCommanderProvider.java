d97c3c9536f63f4397da6ea7cf82cd5f1dd93d10
Jeferson Estevo <jefersonestevo@gmail.com>
2015-08-10 15:40:40 -0300

    A simple example of how to change the prompt dynamically

--- josh-example/src/main/java/josh/example/jcommander/JCommanderProvider.java
+++ josh-example/src/main/java/josh/example/jcommander/JCommanderProvider.java
@@ -6,13 +6,18 @@ import java.util.Collection;
 import josh.command.CommandNotFound;
 import josh.command.jcommander.AbstractJCommanderProvider;
 import josh.command.jcommander.Executable;
+import josh.shell.Shell;
+import josh.shell.ShellAwareCommandProvider;
 
-public class JCommanderProvider extends AbstractJCommanderProvider {
+public class JCommanderProvider extends AbstractJCommanderProvider implements ShellAwareCommandProvider {
+
+    protected Shell shell;
 
     @Override
     protected Collection<Executable> findCommands() {
         Collection<Executable> list = new ArrayList<Executable>();
         list.add(new EchoCommand());
+        list.add(new PromptCommand(shell.getConsoleProvider()));
         return list;
     }
 
@@ -20,8 +25,14 @@ public class JCommanderProvider extends AbstractJCommanderProvider {
     protected Executable getNewCommand(Class<? extends Executable> bean) throws CommandNotFound {
         if (EchoCommand.class.isAssignableFrom(bean)) {
             return new EchoCommand();
+        } else if (PromptCommand.class.isAssignableFrom(bean)) {
+            return new PromptCommand(shell.getConsoleProvider());
         }
         throw new CommandNotFound(bean.getName());
     }
 
+    @Override
+    public void setShell(Shell shell) {
+        this.shell = shell;
+    }
 }
---
+++ josh-example/src/main/java/josh/example/jcommander/PromptCommand.java
@@ -0,0 +1,39 @@
+package josh.example.jcommander;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
+import josh.command.CommandOutcome;
+import josh.command.jcommander.Executable;
+import josh.shell.ConsoleProvider;
+
+@Component
+@Parameters(commandNames = "prompt", commandDescription = "Changes the Prompt Message")
+public class PromptCommand implements Executable {
+
+    private ConsoleProvider consoleProvider;
+
+    public PromptCommand(ConsoleProvider consoleProvider) {
+        this.consoleProvider = consoleProvider;
+    }
+
+    @Parameter
+    private List<String> arguments = new ArrayList<String>();
+
+    @Override
+    public CommandOutcome execute() {
+        if (arguments != null && arguments.size() == 1 && arguments.get(0) != null && !"".equals(arguments.get(0))) {
+            consoleProvider.setPrompt(arguments.get(0));
+        }
+        else {
+            consoleProvider.displayWarning("Plear provide a message to the new prompt");
+        }
+        return new CommandOutcome(0);
+    }
+
+}
